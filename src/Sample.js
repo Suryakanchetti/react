f8ba97c328fe184bd552c19809c994a5aa112778
Suryakanchetti <111098489+Suryakanchetti@users.noreply.github.com>
2025-03-11 12:47:02 +0530

    changes in sample file

--- src/Sample.js
+++ src/Sample.js
@@ -1,18 +1,33 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 
 const Sample = () => {
- const [text, setText] = useState()
+  const [text, setText] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
- const handleClick = () => {
-  setText('Iam here');
- };
-  return  (
-    <div> 
-    <button onClick={handleClick}> Click Me </button>
-    <p>{text}</p>
+  const handleClick = () => {
+    setText('Iam here');
+    console.log('Username:', username, 'Password:', password);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClick}>Login</button>
+      <p>{text}</p>
     </div>
-   
-  )
+  );
 };
 
 export default Sample;  
